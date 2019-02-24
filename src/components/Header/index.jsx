d2770778b4af64947e2b8d5d30e52540ba248292
adannup <dann.tesla@gmail.com>
2019-02-24 13:01:54 -0600

    Add init source for react

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Header = ({ title }) => <h1>{title}</h1>;
+
+Header.defaultProps = {
+  title: 'Title Header'
+};
+
+Header.propTypes = {
+  title: PropTypes.string
+};
+
+export default Header;
---
+++ src/containers/App/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header';
+import './styles/index.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header title="True Home" />
+        <p>True Home</p>
+      </div>
+    );
+  }
+}
+
+export default App;
