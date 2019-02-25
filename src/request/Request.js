aed57a91f49fc506d833469f1b87571c4b63f65f
adannup <dann.tesla@gmail.com>
2019-02-25 03:00:37 -0600

    Add update request on Request Class

--- src/request/Request.js
+++ src/request/Request.js
@@ -1,7 +1,22 @@
 class Request {
   addRequest(uri) {
-    console.log(uri);
     return fetch(uri).then(response => {
+      if (response && response.status === 200) {
+        return response.json();
+      }
+      throw response;
+    });
+  }
+
+  updateRequest(uri, data) {
+    return fetch(uri, {
+      method: 'PATCH',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }).then(response => {
       if (response && response.status === 200) {
         return response.json();
       }
