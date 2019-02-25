8f58aec12c103e1217781b064119a8f69ecb48df
adannup <dann.tesla@gmail.com>
2019-02-25 03:00:05 -0600

    Change props variables on Card Component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import './styles/index.scss';
 
 const Card = props => {
-  const { user, address, image, price, handleDetails } = props;
-  const cardData = { user, address, image, price };
+  const { id, user, address, image, price, handleDetails } = props;
+  const cardData = { id, user, address, image, price };
 
   return (
     <div className="col-md-4 my-2" onClick={event => handleDetails(cardData)}>
       <div className="card card-propiedad">
         <img className="card-img-top" src={image} alt={address} />
         <div className="card-body">
-          <h5 className="card-title text-danger">{price}</h5>
+          <h5 className="card-title text-danger">{`$ ${price}`}</h5>
           <p className="card-text">{address}</p>
           <p className="card-text text-secondary">{`Contacto: ${user.fullname}`}</p>
         </div>
@@ -21,6 +21,7 @@ const Card = props => {
 };
 
 Card.propTypes = {
+  id: PropTypes.number.isRequired,
   address: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   user: PropTypes.shape({
