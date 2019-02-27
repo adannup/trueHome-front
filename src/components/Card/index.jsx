a45563ff1beb231eca40052f4f5ddf60188f0d87
adannup <dann.tesla@gmail.com>
2019-02-27 11:52:27 -0600

    Add Card component

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './styles/index.scss';
+
+const Card = props => {
+  const { user, address, image, price, handleDetails } = props;
+  const cardData = { user, address, image, price };
+
+  return (
+    <div className="col-md-4 my-2" onClick={event => handleDetails(cardData)}>
+      <div className="card card-propiedad">
+        <img className="card-img-top" src={image} alt={address} />
+        <div className="card-body">
+          <h5 className="card-title text-danger">{price}</h5>
+          <p className="card-text">{address}</p>
+          <p className="card-text text-secondary">{`Contacto: ${user.fullname}`}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  address: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  user: PropTypes.shape({
+    fullname: PropTypes.string,
+    username: PropTypes.string,
+    phone: PropTypes.string
+  }).isRequired,
+  handleDetails: PropTypes.func.isRequired,
+  price: PropTypes.string.isRequired
+};
+
+export default Card;
